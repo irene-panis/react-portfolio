1700d4ddca8d6742dc3f523aa6a7c48507c847a5
irene <panisirene@gmail.com>
2023-08-28 21:16:48 -0700

    construct pages and add dummy content

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function AboutMe() {
+  return (
+    <section className='about'>
+      <h2>Who I Am</h2>
+      <div className='about-content'>
+        <p>
+          I'm Irene Panis, a web developer and software engineer based in Southern California.
+        </p>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,70 @@
+import ProjectCard from '../components/Project';
+
+export default function Projects() {
+  const projects = [
+    {
+      title: 'Dagobah Depot',
+      description: 'A Star Wars-themed community marketplace built with two other team members. Full-stack application.',
+      stack: 'Sequelize ORM, Node.js, Express.js, Handlebars.js, Stripe.js',
+      src: '',
+      repo: 'https://github.com/irene-panis/dagobah-depot',
+      live: 'https://dagobah-depot-34081fe1df5e.herokuapp.com/'
+    },
+    {
+      title: 'Pokedex',
+      description: 'A simple application for creating Pokemon teams and viewing Pokemon information.',
+      stack: 'HTML, Tailwind CSS, JavaScript, jQuery, RESTful APIs',
+      src: '',
+      repo: 'https://github.com/irene-panis/pokemon-index',
+      live: 'https://linuscth.github.io/pokemon-index/'
+    },
+    {
+      title: 'Ensemble Blog',
+      description: 'Simple blog that allows users to sign up or log in and create posts and comments. Full-stack application.',
+      stack: 'Bulma CSS, Node.js, Express.js, Sequelize ORM, Handlebars.js',
+      src: '',
+      repo: 'https://github.com/irene-panis/ensemble-blog',
+      live: 'https://ensemble-blog-bfd105093377.herokuapp.com/'
+    },
+    {
+      title: 'The Weather App',
+      description: 'A small weather app that displays current weather info and five-day forecast for any given city using Weather API.',
+      stack: 'HTML, Bootstrap CSS, JavaScript, jQuery, RESTful APIs, Day.js',
+      src: '',
+      repo: 'https://github.com/irene-panis/weather-app',
+      live: 'https://irene-panis.github.io/weather-app/'
+    },
+    {
+      title: 'JavaScript Quiz',
+      description: 'Quiz app that asks JavaScript questions and allows users to save their scores in local storage.',
+      stack: 'HTML, CSS, JavaScript',
+      src: '',
+      repo: 'https://github.com/irene-panis/javascript-quiz',
+      live: 'https://irene-panis.github.io/javascript-quiz/'
+    },
+    {
+      title: 'Portfolio',
+      description: 'An old version of my portfolio created in April 2023.',
+      stack: 'HTML, CSS',
+      src: '',
+      repo: 'https://github.com/irene-panis/portfolio',
+      live: 'https://irene-panis.github.io/portfolio/'
+    },
+  ];
+
+  return (
+    <div className="projects-list">
+      {projects.map((project, index) => (
+        <ProjectCard
+          key={index}
+          title={project.title}
+          description={project.description}
+          stack={project.stack}
+          src={project.src}
+          repo={project.repo}
+          live={project.live}
+        />
+      ))}
+    </div>
+  );
+}
