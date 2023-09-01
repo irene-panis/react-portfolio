ef2ec7e6cfb69025996ff5802e563b8dd291e316
irene <panisirene@gmail.com>
2023-09-01 15:13:18 -0700

    add hover animations to social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <footer className="mb-3">
       <div className="flex justify-center align-center gap-6">
-        <SocialIcon target="_blank" rel="noreferrer" url="https://github.com/irene-panis"/>
-        <SocialIcon target="_blank" rel="noreferrer" url="https://www.linkedin.com/in/irene-panis/"/>
-        <SocialIcon target="_blank" rel="noreferrer" url="https://discordapp.com/users/354433105807015957"/>
+        <SocialIcon className="brightness-0 hover:brightness-200 hover:saturate-0 ease-in-out duration-300" target="_blank" rel="noreferrer" url="https://github.com/irene-panis"/>
+        <SocialIcon className="brightness-0 hover:brightness-200 hover:saturate-0 ease-in-out duration-300" target="_blank" rel="noreferrer" url="https://www.linkedin.com/in/irene-panis/"/>
+        <SocialIcon className="brightness-0 hover:brightness-200 hover:saturate-0 ease-in-out duration-300" target="_blank" rel="noreferrer" url="https://discordapp.com/users/354433105807015957"/>
       </div>
     </footer>
   )
