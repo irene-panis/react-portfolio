53b16717513152afdb0db77d36e7941bb407c270
irene <panisirene@gmail.com>
2023-08-31 21:24:15 -0700

    add footer margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { SocialIcon } from 'react-social-icons';
 
 function Footer() {
   return (
-    <footer>
-      <div className="socials-icons flex justify-center gap-6">
+    <footer className="mb-3">
+      <div className="flex justify-center align-center gap-6">
         <SocialIcon url="https://github.com/irene-panis"/>
         <SocialIcon url="https://www.linkedin.com/in/irene-panis/"/>
         <SocialIcon url="https://discordapp.com/users/354433105807015957"/>
