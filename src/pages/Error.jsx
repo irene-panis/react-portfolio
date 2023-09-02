dfa5afd1c774564c77ec032569a2c1625f9e87eb
irene <panisirene@gmail.com>
2023-09-01 22:43:30 -0700

    create quick error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useRouteError } from 'react-router-dom';
+
 export default function Error() {
+  const error = useRouteError();
   return (
-    <span>Error</span>
+    <div className="flex flex-col align-items">
+      <h2>Uh oh!</h2>
+      <h3>The page you were looking for couldn't be found...</h3>
+      <span>{error.message}</span>
+    </div>
   );
 }
