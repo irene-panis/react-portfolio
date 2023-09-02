cb4e51a71cbed50889c20de9b88f72ccefe5ce4f
irene <panisirene@gmail.com>
2023-09-01 23:09:43 -0700

    fix current page highlight

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,13 +2,14 @@ import { Link, useLocation } from 'react-router-dom';
 
 function Navigation() {
   const currentPage = useLocation().pathname;
+  console.log(currentPage);
 
   return ( 
     <ul className="flex flex-wrap gap-20 uppercase">
       <li>
         <Link 
           to="/"
-          className={`hover:underline ${currentPage === 'AboutMe' ? 'bg-white text-black py-1 px-2 rounded' : 'bg-none text-white py-1 px-2 rounded'}`}
+          className={`hover:underline ${currentPage === '/' ? 'bg-white text-black py-1 px-2 rounded' : 'bg-none text-white py-1 px-2 rounded'}`}
         >
           About Me
         </Link>
@@ -16,7 +17,7 @@ function Navigation() {
       <li>
         <Link 
           to="/Projects"
-          className={`hover:underline ${currentPage === 'Projects' ? 'bg-white text-black py-1 px-2 rounded' : 'bg-none text-white py-1 px-2 rounded'}`}
+          className={`hover:underline ${currentPage === '/Projects' ? 'bg-white text-black py-1 px-2 rounded' : 'bg-none text-white py-1 px-2 rounded'}`}
         >
           Projects
         </Link>
@@ -24,7 +25,7 @@ function Navigation() {
       <li>
         <Link 
           to="/Contact"
-          className={`hover:underline ${currentPage === 'Contact' ? 'bg-white text-black py-1 px-2 rounded' : 'bg-none text-white py-1 px-2 rounded'}`}
+          className={`hover:underline ${currentPage === '/Contact' ? 'bg-white text-black py-1 px-2 rounded' : 'bg-none text-white py-1 px-2 rounded'}`}
         >
           Contact
         </Link>
@@ -32,7 +33,7 @@ function Navigation() {
       <li>
         <Link 
           to="/Resume"
-          className={`hover:underline ${currentPage === 'Resume' ? 'bg-white text-black py-1 px-2 rounded' : 'bg-none text-white py-1 px-2 rounded'}`}
+          className={`hover:underline ${currentPage === '/Resume' ? 'bg-white text-black py-1 px-2 rounded' : 'bg-none text-white py-1 px-2 rounded'}`}
         >
           Resume
         </Link>
