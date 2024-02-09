5de95d617474489ecd20ce23707b68fec136cd2d
irene <panisirene@gmail.com>
2024-02-09 00:20:30 -0800

    change contact form color

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -69,29 +69,29 @@ export default function Contact() {
   return (
     <div className="contact flex flex-col gap-5 lg:w-1/3 w-5/6">
       <h2 className="text-2xl">Let's chat!</h2>
-      <form id="contact-form" className="flex flex-col gap-5" action="https://api.web3forms.com/submit" method="POST">
+      <form id="contact-form" className="flex flex-col gap-5 bg-white p-5 text-black rounded-md shadow-md" action="https://api.web3forms.com/submit" method="POST">
 
         <input type="hidden" name="access_key" value="a499c407-de41-4e17-b58b-1ec11eb4a622"/>
         <input type="hidden" name="redirect" value="https://irenepanis.netlify.app/Contact"></input>
         <div className="flex flex-col">
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" className="rounded-lg text-black pl-2" name="inputName" value={inputName} onChange={handleInputChange} onBlur={handleBlur} required />
+          <input type="text" id="name" className="rounded-lg text-black pl-2 border border-stone-400" name="inputName" value={inputName} onChange={handleInputChange} onBlur={handleBlur} required />
           {errors.inputName && <p className="error">{errors.inputName}</p>}
         </div>
         
         <div className="flex flex-col">
           <label htmlFor="email">Email Address:</label>
-          <input type="email" id="email" className="rounded-lg text-black pl-2" name="email" value={email} onChange={handleInputChange} onBlur={handleBlur} required />
+          <input type="email" id="email" className="rounded-lg text-black pl-2 border border-stone-400" name="email" value={email} onChange={handleInputChange} onBlur={handleBlur} required />
           {errors.email && <p className="error">{errors.email}</p>}
         </div>
 
         <div className="flex flex-col">
           <label htmlFor="message">Message:</label>
-          <textarea id="message" className="rounded-lg text-black pl-2" name="message" value={message} rows="4" onChange={handleInputChange} onBlur={handleBlur} required></textarea>
+          <textarea id="message" className="rounded-lg text-black pl-2 border border-stone-400" name="message" value={message} rows="4" onChange={handleInputChange} onBlur={handleBlur} required></textarea>
           {errors.message && <p className="error">{errors.message}</p>}
         </div>
         
-        <input type="submit" value="Submit" className="bg-white text-black w-1/5 rounded-lg font-bold hover:bg-black hover:text-white py-1"/>
+        <input type="submit" value="Submit" className="bg-black text-white w-1/5 rounded-lg font-bold hover:bg-stone-600 duration-200 ease-in-out py-1"/>
       </form>
       <script src="https://web3forms.com/client/script.js" async defer></script>
     </div>
